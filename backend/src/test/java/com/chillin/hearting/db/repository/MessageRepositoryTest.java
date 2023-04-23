a680cd0b9ecc9806450d7a33ff6459a62e9071c5
Youngmook Lim <youngmookk@gmail.com>
2023-04-24 04:25:28 +0900

    fix: fix method names for test methods

--- backend/src/test/java/com/chillin/hearting/db/repository/MessageRepositoryTest.java
+++ backend/src/test/java/com/chillin/hearting/db/repository/MessageRepositoryTest.java
@@ -25,12 +25,12 @@ public class MessageRepositoryTest {
     private EmojiRepository emojiRepository;
 
     @Test
-    public void MessageRepositoryIsNotNull() {
+    public void messageRepositoryIsNotNull() {
         assertThat(messageRepository).isNotNull();
     }
 
     @Test
-    public void SendMessage() {
+    public void sendMessage() {
         // given
         Heart heart = new Heart(null, "testHeart", "testHeartUrl", "short", "long", "type", "acq");
         User sender = new User("1", "KAKAO", "test1@test.com", "test1", "refresh1", null, null, "status", 0, 'A', "role", 0L);
