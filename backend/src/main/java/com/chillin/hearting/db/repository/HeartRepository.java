8cbe722952cf33c6b43c65bf7def6b2941520afb
손민혁 <sonmh79@naver.com>
2023-04-24 03:12:18 +0900

    feat: add heart repository

---
+++ backend/src/main/java/com/chillin/hearting/db/repository/HeartRepository.java
@@ -0,0 +1,7 @@
+package com.chillin.hearting.db.repository;
+
+import com.chillin.hearting.db.domain.Heart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeartRepository extends JpaRepository<Heart,Long> {
+}
