1dff69ca2e023c8a6a52e2c0ec1c69bfde6d2e7c
Youngmook Lim <youngmookk@gmail.com>
2023-04-25 04:19:19 +0900

    feat: add controller and controller test for messages

--- backend/src/main/java/com/chillin/hearting/api/controller/MessageController.java
+++ backend/src/main/java/com/chillin/hearting/api/controller/MessageController.java
@@ -1,13 +1,36 @@
 package com.chillin.hearting.api.controller;
 
+import com.chillin.hearting.api.request.SendMessageReq;
+import com.chillin.hearting.api.response.SendMessageRes;
+import com.chillin.hearting.db.domain.User;
+import com.chillin.hearting.exception.WrongUserException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/messages")
 @RequiredArgsConstructor
-public class MessageControllerTest {
+public class MessageController {
+
+    @PostMapping("/")
+    public ResponseEntity<SendMessageRes> sendMessage(@Valid @RequestBody SendMessageReq sendMessageReq, HttpServletRequest httpServletRequest) {
+        User user = (User) httpServletRequest.getAttribute("user");
+
+        // Check if logged in user is same as sender
+        if (!user.getId().equals(sendMessageReq.getSenderId())) {
+            throw new WrongUserException();
+        }
+
+        return null;
+    }
+
 }
--- backend/src/test/java/com/chillin/hearting/api/controller/MessageControllerTest.java
+++ backend/src/test/java/com/chillin/hearting/api/controller/MessageControllerTest.java
@@ -1,4 +1,69 @@
 package com.chillin.hearting.api.controller;
 
+import com.chillin.hearting.api.request.SendMessageReq;
+import com.chillin.hearting.api.service.MessageService;
+import com.chillin.hearting.db.domain.User;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(MockitoExtension.class)
 public class MessageControllerTest {
+
+    @InjectMocks
+    private MessageController messageController;
+
+    private MockMvc mockMvc;
+    private Gson gson;
+
+    @Mock
+    private MessageService messageService;
+
+    @BeforeEach
+    public void init() {
+        gson = new Gson();
+        mockMvc = MockMvcBuilders.standaloneSetup(messageController).build();
+    }
+
+    @Test
+    public void failSendMessage_InvalidUser() throws Exception {
+        // given
+        final String url = "/api/v1/messages";
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        User user = User.builder().id("otherSender").build();
+        when(req.getAttribute("user")).thenReturn(user);
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .content(gson.toJson(SendMessageReq.builder().heartId(0L).senderId("sender").receiverId("receiver").title("title").build()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("user", user);
+                            return request;
+                        })
+        );
+
+        // then
+        resultActions.andExpect(status().isUnauthorized());
+
+    }
+
+
 }
