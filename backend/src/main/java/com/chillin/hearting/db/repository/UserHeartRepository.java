05afec329a310f10684a7ae459adb25a48f33966
손민혁 <sonmh79@naver.com>
2023-04-25 02:55:26 +0900

    feat: add find by user method

--- backend/src/main/java/com/chillin/hearting/db/repository/UserHeartRepository.java
+++ backend/src/main/java/com/chillin/hearting/db/repository/UserHeartRepository.java
@@ -1,7 +1,12 @@
 package com.chillin.hearting.db.repository;
 
+import com.chillin.hearting.db.domain.User;
 import com.chillin.hearting.db.domain.UserHeart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserHeartRepository extends JpaRepository<UserHeart, Long> {
+
+    public List<UserHeart> findByUser(User user);
 }
