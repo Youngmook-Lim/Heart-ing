3c070bd1d28c80aac6e39ef8c3ecf7f2b28d0f9b
손민혁 <sonmh79@naver.com>
2023-04-25 02:58:16 +0900

    feat: add heart serive logic

---
+++ backend/src/main/java/com/chillin/hearting/api/service/HeartService.java
@@ -0,0 +1,60 @@
+package com.chillin.hearting.api.service;
+
+import com.chillin.hearting.api.response.HeartRes;
+import com.chillin.hearting.db.domain.Heart;
+import com.chillin.hearting.db.domain.User;
+import com.chillin.hearting.db.domain.UserHeart;
+import com.chillin.hearting.db.repository.HeartRepository;
+import com.chillin.hearting.db.repository.UserHeartRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@Slf4j
+@RequiredArgsConstructor
+public class HeartService {
+
+    private HeartRepository heartRepository;
+
+    private UserHeartRepository userHeartRepository;
+
+    private final String DEFAULT_TYPE = "DEFAULT";
+
+    /**
+     * 모든 도감 리스트를 반환합니다.
+     * 로그인 사용자는 하트 획득 조건을 달성한 도감에 대해 잠금이 해제됩니다.
+     * 비로그인 사용자이면 DEFAULT 타입의 도감만 잠금이 해제됩니다.
+     * @param user
+     * @return 하트 DTO
+     */
+    public List<HeartRes> findAllHearts(User user) {
+        List<Heart> findHearts = heartRepository.findAll();
+        List<HeartRes> resHearts = new ArrayList<>();
+        for (Heart heart : findHearts) {
+            resHearts.add(HeartRes.builder()
+                    .heart(heart)
+                    .isLocked((heart.getType() == DEFAULT_TYPE) ? false : true)
+                    .build());
+        }
+
+        if (user != null) {
+            List<UserHeart> userHearts = userHeartRepository.findByUser(user);
+            for (HeartRes heartRes : resHearts) {
+                for (UserHeart myHeart : userHearts) {
+                    if (heartRes.getType() != DEFAULT_TYPE && heartRes.getId() == myHeart.getHeart().getId()) {
+                        heartRes.unLock();
+                        break;
+                    }
+                }
+            }
+        }
+
+        return resHearts;
+    }
+}
