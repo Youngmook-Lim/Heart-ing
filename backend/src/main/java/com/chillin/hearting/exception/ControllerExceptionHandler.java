8bbad6902a51462111dabe1b434c08f02b710e7e
Youngmook Lim <youngmookk@gmail.com>
2023-04-25 04:18:26 +0900

    feat: add WrongUserException to handler

--- backend/src/main/java/com/chillin/hearting/exception/ControllerExceptionHandler.java
+++ backend/src/main/java/com/chillin/hearting/exception/ControllerExceptionHandler.java
@@ -2,11 +2,22 @@ package com.chillin.hearting.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
+
+    @ExceptionHandler(WrongUserException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ResponseBody
+    public ErrorRes handleWrongUserException(WrongUserException e) {
+        log.error(e.getMessage());
+        return ErrorRes.make(e.getMessage());
+    }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
