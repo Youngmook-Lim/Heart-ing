85ef37a8bc36b9bad9518e583612987bbb620a3f
손민혁 <sonmh79@naver.com>
2023-04-25 02:56:49 +0900

    fix: fix test code of heart repository

--- backend/src/test/java/com/chillin/hearting/db/repository/HeartRepositoryTest.java
+++ backend/src/test/java/com/chillin/hearting/db/repository/HeartRepositoryTest.java
@@ -3,6 +3,7 @@ package com.chillin.hearting.db.repository;
 import com.chillin.hearting.db.domain.Heart;
 import com.chillin.hearting.db.domain.User;
 import com.chillin.hearting.exception.NotFoundException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -21,14 +22,58 @@ public class HeartRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
-    public Heart createHeart() {
+    private Heart savedDefaultHeart;
+    private Heart savedSpecialHeart;
+    private User savedUser;
+
+    @BeforeEach
+    public void 데이터생성() {
+        Heart defaultHeart = createDefaultHeart();
+        Heart specialHeart = createSpecialHeart();
+        User user = createUser();
+
+        savedDefaultHeart = heartRepository.save(defaultHeart);
+        savedSpecialHeart = heartRepository.save(specialHeart);
+        savedUser = userRepository.save(user);
+    }
+
+
+    @Test
+    public void 모든도감조회() {
+        // when
+        List<Heart> heartList = heartRepository.findAll();
+
+        // then
+        assertThat(heartList)
+                .anySatisfy(heart -> {
+                    assertThat(heart.getName()).isEqualTo(savedDefaultHeart.getName());
+                });
+
+        assertThat(heartList).extracting("type")
+                .containsOnly("DEFAULT","SPECIAL");
+    }
+
+    public Heart createDefaultHeart() {
         Heart heart = Heart.builder()
-                .name("테스트하트")
+                .name("호감 하트")
                 .imageUrl("test.com")
-                .shortDescription("short test dsc")
-                .longDescription("long test dsc")
-                .acqCondition("test condition")
-                .type("DEFAUT")
+                .shortDescription("짧은 설명 !")
+                .longDescription("호감의 탄생 스토리")
+                .acqCondition("기본 제공")
+                .type("DEFAULT")
+                .build();
+
+        return heart;
+    }
+
+    public Heart createSpecialHeart() {
+        Heart heart = Heart.builder()
+                .name("행성 하트")
+                .imageUrl("universe.com")
+                .shortDescription("우주에 단 하나 뿐인 너 !")
+                .longDescription("우주의 탄생 스토리")
+                .acqCondition("특정인에게 5회 이상 메시지 전송")
+                .type("SPECIAL")
                 .build();
 
         return heart;
@@ -42,48 +87,5 @@ public class HeartRepositoryTest {
                 .nickname("test-nick")
                 .build();
         return user;
-    }
-
-    @Test
-    public void 도감생성() {
-        // given
-        Heart createHeart = createHeart();
-
-        // when
-        Heart savedHeart = heartRepository.save(createHeart);
-        Heart findHeart = heartRepository.findById(savedHeart.getId()).orElseThrow(NotFoundException::new);
-
-        // then
-        assertThat(findHeart.getId())
-                .isEqualTo(savedHeart.getId());
-    }
-
-    @Test
-    public void 도감획득() {
-        // given
-        Heart createHeart = createHeart();
-        User createUser = createUser();
-
-        // when
-        Heart savedHeart = heartRepository.save(createHeart);
-        User savedUser = userRepository.save(createUser);
-
-    }
-
-    @Test
-    public void 모든도감조회() {
-
-        // given
-        Heart createHeart = createHeart();
-
-        // when
-        Heart savedHeart = heartRepository.save(createHeart);
-        List<Heart> heartList = heartRepository.findAll();
-
-        // then
-        assertThat(heartList)
-                .anySatisfy(heart -> {
-                    assertThat(heart.getName()).isEqualTo(savedHeart.getName());
-                });
     }
 }
