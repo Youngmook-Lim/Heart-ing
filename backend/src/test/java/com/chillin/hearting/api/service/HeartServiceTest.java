c05dcf2beda982b70338cbef6c4fb8168ddb533f
sonmh79 <sonmh79@naver.com>
2023-04-24 17:56:58 +0900

    feat: add heart service test code

---
+++ backend/src/test/java/com/chillin/hearting/api/service/HeartServiceTest.java
@@ -0,0 +1,46 @@
+package com.chillin.hearting.api.service;
+
+import com.chillin.hearting.db.domain.Heart;
+import com.chillin.hearting.db.repository.HeartRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class HeartServiceTest {
+
+    @InjectMocks
+    private HeartService heartService;
+
+    @Mock
+    private HeartRepository heartRepository;
+
+    @Test
+    public void 도감조회() {
+
+        // given
+        Heart heart1 = Heart.builder()
+                .name("heart1")
+                .imageUrl("test-url1.com")
+                .shortDescription("short-dsc1")
+                .longDescription("long-dsc1")
+                .acqCondition("test-condition1")
+                .type("DEFAUT")
+                .build();
+        
+        // moking
+        when(heartRepository.save(any(Heart.class))).thenReturn(heart1);
+
+
+        // when
+
+        // then
+    }
+
+
+}
