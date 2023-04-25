4a39266f7af7dd7d75173b1f56106ab51cf81b58
Youngmook Lim <youngmookk@gmail.com>
2023-04-25 13:32:29 +0900

    feat: add success send message test

--- backend/src/test/java/com/chillin/hearting/api/controller/MessageControllerTest.java
+++ backend/src/test/java/com/chillin/hearting/api/controller/MessageControllerTest.java
@@ -1,6 +1,7 @@
 package com.chillin.hearting.api.controller;
 
 import com.chillin.hearting.api.request.SendMessageReq;
+import com.chillin.hearting.api.response.SendMessageRes;
 import com.chillin.hearting.api.service.MessageService;
 import com.chillin.hearting.db.domain.User;
 import com.chillin.hearting.exception.ControllerExceptionHandler;
@@ -20,10 +21,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -104,6 +105,50 @@ public class MessageControllerTest {
     }
 
     @Test
+    public void successSendMessage() throws Exception {
+        // given
+        final String url = "/api/v1/messages/";
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        User user = User.builder().id("sender").build();
+
+        SendMessageReq sendMessageReq = SendMessageReq.builder()
+                .heartId(0L)
+                .senderId("sender")
+                .receiverId("receiver")
+                .title("title")
+                .build();
+
+        SendMessageRes expectedResponse = SendMessageRes.builder()
+                .data(Map.of(
+                        "messageId", 1L,
+                        "heartId", 0L,
+                        "heartName", "exampleHeartName",
+                        "heartUrl", "exampleHeartUrl",
+                        "isRead", false
+                ))
+                .build();
+
+        doReturn(expectedResponse).when(messageService).sendMessage(sendMessageReq.getHeartId(), sendMessageReq.getSenderId(), sendMessageReq.getReceiverId(), sendMessageReq.getTitle(), null, null);
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .content(gson.toJson(sendMessageReq))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("user", user);
+                            return request;
+                        })
+        );
+
+        // then
+        resultActions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.messageId", is(1)))
+                .andExpect(jsonPath("$.data.heartId", is(0)))
+                .andExpect(jsonPath("$.data.heartName", is("exampleHeartName")))
+                .andExpect(jsonPath("$.data.heartUrl", is("exampleHeartUrl")))
+                .andExpect(jsonPath("$.data.isRead", is(false)));
+    }
 
 
 }
