693e38730e0c1782918b667e6f5d66eb1d4d655a
sonmh79 <sonmh79@naver.com>
2023-04-25 11:18:38 +0900

    refactor: refactor userheart repo test

--- backend/src/test/java/com/chillin/hearting/db/repository/UserHeartRepositoryTest.java
+++ backend/src/test/java/com/chillin/hearting/db/repository/UserHeartRepositoryTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
