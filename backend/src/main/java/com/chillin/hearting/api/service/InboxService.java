06776d5b1d2e21c1748100617bf0eee885dc6491
손민혁 <sonmh79@naver.com>
2023-04-26 02:19:32 +0900

    feat: add find inbox messages method

--- backend/src/main/java/com/chillin/hearting/api/service/InboxService.java
+++ backend/src/main/java/com/chillin/hearting/api/service/InboxService.java
@@ -1,8 +1,12 @@
 package com.chillin.hearting.api.service;
 
+import com.chillin.hearting.api.data.InboxData;
 import com.chillin.hearting.db.domain.Message;
+import com.chillin.hearting.db.domain.User;
 import com.chillin.hearting.db.repository.InboxRepository;
+import com.chillin.hearting.db.repository.UserRepository;
 import com.chillin.hearting.exception.MessageNotFoundException;
+import com.chillin.hearting.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,10 +18,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class InboxService {
 
-    private InboxRepository inboxRepository;
+    private final InboxRepository inboxRepository;
+    private final UserRepository userRepository;
 
     public void storeMessage(Long messageId) {
         Message findMessage = inboxRepository.findById(messageId).orElseThrow(MessageNotFoundException::new);
         findMessage.toInbox();
+    }
+
+    public InboxData findInboxMessages(String userId) {
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        return InboxData.builder().inboxList(inboxRepository.findAllByReceiverAndIsStored(user, true)).build();
     }
 }
