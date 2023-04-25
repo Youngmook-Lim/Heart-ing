34d2b909dc72ccf3dd6f86475fc07c1fd8af6f6e
손민혁 <sonmh79@naver.com>
2023-04-26 02:18:07 +0900

    feat: add find inbox list method

--- backend/src/main/java/com/chillin/hearting/db/repository/InboxRepository.java
+++ backend/src/main/java/com/chillin/hearting/db/repository/InboxRepository.java
@@ -1,7 +1,12 @@
 package com.chillin.hearting.db.repository;
 
 import com.chillin.hearting.db.domain.Message;
+import com.chillin.hearting.db.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InboxRepository extends JpaRepository<Message, Long> {
+
+    public List<Message> findAllByReceiverAndIsStored(User user,Boolean isStored);
 }
