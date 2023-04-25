af7b77b41290f21f83bc4e45593bc994a591bd24
손민혁 <sonmh79@naver.com>
2023-04-26 03:14:07 +0900

    feat: add inbox detail repo method

--- backend/src/main/java/com/chillin/hearting/db/repository/InboxRepository.java
+++ backend/src/main/java/com/chillin/hearting/db/repository/InboxRepository.java
@@ -5,8 +5,11 @@ import com.chillin.hearting.db.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InboxRepository extends JpaRepository<Message, Long> {
 
-    public List<Message> findAllByReceiverAndIsStored(User user,Boolean isStored);
+    public List<Message> findAllByReceiverAndIsStored(User user, Boolean isStored);
+
+    public Optional<Message> findByIdAndReceiverAndIsStored(Long id, User user, Boolean isStored);
 }
