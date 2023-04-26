e2da28079320d2ac7ff270cf76f4fb6e906e3259
seohyeonkyeong <hkyung99@naver.com>
2023-04-27 03:21:44 +0900

    feat: main theme and color

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App bg-hrtColorBackground">
+    <div className="App bg-hrtColorBackground text-hrtColorOutline">
       <BrowserRouter>
         <Navbar />
         <Routes>
