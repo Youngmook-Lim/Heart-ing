b1cd87b0f6f99a1d5f68d6fcc662665a0b9661ae
Youngmook Lim <youngmookk@gmail.com>
2023-04-26 15:23:49 +0900

    refactor: refactor Data objects

---
+++ backend/src/main/java/com/chillin/hearting/api/data/MessageData.java
@@ -0,0 +1,38 @@
+package com.chillin.hearting.api.data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MessageData extends Data {
+
+    private long messageId;
+    private String title;
+    private long heartId;
+    private String heartName;
+    private String heartUrl;
+    private long emojiId;
+    private String emojiName;
+    private String emojiUrl;
+    private LocalDateTime createdDate;
+    private LocalDateTime expiredDate;
+    private String shortDescription;
+
+    @Getter(onMethod_ = {@JsonProperty("isRead")})
+    @Setter(onMethod_ = {@JsonProperty("isRead")})
+    private boolean isRead;
+    @Getter(onMethod_ = {@JsonProperty("isReported")})
+    @Setter(onMethod_ = {@JsonProperty("isReported")})
+    private boolean isReported;
+    @Getter(onMethod_ = {@JsonProperty("isStored")})
+    @Setter(onMethod_ = {@JsonProperty("isStored")})
+    private boolean isStored;
+
+}
--- backend/src/main/java/com/chillin/hearting/api/data/SendMessageData.java
+++ backend/src/main/java/com/chillin/hearting/api/data/SendMessageData.java
@@ -8,7 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class MessageData extends Data {
+public class SendMessageData extends Data {
 
     private long messageId;
     private long heartId;
