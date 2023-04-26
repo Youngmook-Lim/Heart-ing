6a1662caadb194d258dcb15d1e6580f8bd91f056
sonmh79 <sonmh79@naver.com>
2023-04-26 15:06:53 +0900

    fix: fix inboxdata

--- backend/src/main/java/com/chillin/hearting/api/data/InboxData.java
+++ backend/src/main/java/com/chillin/hearting/api/data/InboxData.java
@@ -1,16 +1,17 @@
 package com.chillin.hearting.api.data;
 
-import com.chillin.hearting.db.domain.Message;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.util.List;
 
 @Getter
+@Setter
 @ToString
 public class InboxData extends Data {
-    private List<Message> inboxList;
+    private List<InboxDTO> inboxList;
 
     @Builder
     public InboxData(List inboxList) {
