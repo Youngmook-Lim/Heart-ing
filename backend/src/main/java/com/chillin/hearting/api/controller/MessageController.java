9d655a5f8a32172e319cd72209c9dbf9698808fb
Youngmook Lim <youngmookk@gmail.com>
2023-04-26 16:46:18 +0900

    feat: implement get message detail on controller

--- backend/src/main/java/com/chillin/hearting/api/controller/MessageController.java
+++ backend/src/main/java/com/chillin/hearting/api/controller/MessageController.java
@@ -145,4 +145,28 @@ public class MessageController {
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
+    @GetMapping("/received/detail/{messageId}")
+    public ResponseEntity<ResponseDTO> getMessageDetail(@PathVariable("messageId") long messageId, HttpServletRequest httpServletRequest) {
+
+        User user = (User) httpServletRequest.getAttribute("user");
+
+        if (user == null) {
+            throw new UnAuthorizedException();
+        }
+
+        Data data = messageService.getMessageDetail(messageId, user.getId());
+
+        if (data == null) {
+            throw new MessageDetailFailException();
+        }
+
+        ResponseDTO responseDTO = ResponseDTO.builder()
+                .status(SUCCESS)
+                .message("메시지 상세가 성공적으로 반환되었습니다.")
+                .data(data)
+                .build();
+
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
 }
