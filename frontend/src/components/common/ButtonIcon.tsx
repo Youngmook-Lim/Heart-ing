b2e6a06ce3d8208da54af0719adb7a3fed13efd3
seohyeonkyeong <hkyung99@naver.com>
2023-04-27 03:17:39 +0900

    feat: common button component

---
+++ frontend/src/components/common/ButtonIcon.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { ReactComponent as ClosePurple } from "../../assets/images/pixel/button/close_purple_1.svg";
+
+type propType = { id: number };
+
+function ButtonIcon(id: propType) {
+  const index = id.id;
+  const IconList = [<ClosePurple className="w-8 h-8" />];
+  return IconList[index];
+}
+
+export default ButtonIcon;
