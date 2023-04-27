d69ba05db252530928ad0c92c97b4b8fea34ea6d
sonmh79 <sonmh79@naver.com>
2023-04-27 17:31:42 +0900

    feat: add all heart service logic

--- backend/src/main/java/com/chillin/hearting/api/service/HeartService.java
+++ backend/src/main/java/com/chillin/hearting/api/service/HeartService.java
@@ -6,12 +6,14 @@ import com.chillin.hearting.db.domain.User;
 import com.chillin.hearting.db.domain.UserHeart;
 import com.chillin.hearting.db.repository.HeartRepository;
 import com.chillin.hearting.db.repository.UserHeartRepository;
+import com.chillin.hearting.db.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -24,38 +26,42 @@ public class HeartService {
 
     private UserHeartRepository userHeartRepository;
 
+    private UserRepository userRepository;
+
     private final String DEFAULT_TYPE = "DEFAULT";
 
     /**
      * 모든 도감 리스트를 반환합니다.
-     * 로그인 사용자는 하트 획득 조건을 달성한 도감에 대해 잠금이 해제됩니다.
-     * 비로그인 사용자이면 DEFAULT 타입의 도감만 잠금이 해제됩니다.
+     * DEFAULT 타입의 도감은 잠금이 해제됩니다.
+     * 로그인 사용자는 추가적으로 하트 획득 조건을 달성한 하트에 대해 잠금이 해제됩니다.
      *
      * @param user
      * @return 하트 DTO
      */
     public List<HeartData> findAllHearts(User user) {
-        List<Heart> findHearts = heartRepository.findAll();
+        // DB의 모든 하트를 조회한다. 기본하트는 isLocked = false
+        List<Heart> allHearts = heartRepository.findAll();
         List<HeartData> resHearts = new ArrayList<>();
-        for (Heart heart : findHearts) {
-            resHearts.add(HeartData.builder()
-                    .heart(heart)
-                    .isLocked((heart.getType() == DEFAULT_TYPE) ? false : true)
-                    .build());
+        for (Heart heart : allHearts) {
+            resHearts.add(HeartData.of(heart, (heart.getType() == DEFAULT_TYPE) ? false : true));
         }
 
+        HashSet<Long> hashSet = new HashSet<>();
         if (user != null) {
-            List<UserHeart> userHearts = userHeartRepository.findByUser(user);
+            String userId = user.getId();
+            userRepository.findById(userId);
+            List<UserHeart> userHearts = userHeartRepository.findAllByUserId(userId);
+            for (UserHeart myHeart : userHearts) {
+                hashSet.add(myHeart.getId());
+            }
+
             for (HeartData heartData : resHearts) {
-                for (UserHeart myHeart : userHearts) {
-                    if (heartData.getType() != DEFAULT_TYPE && heartData.getId() == myHeart.getHeart().getId()) {
-                        heartData.unLock();
-                        break;
-                    }
+                if (heartData.getType() != DEFAULT_TYPE && hashSet.contains(heartData.getId())) {
+                    heartData.unLock();
                 }
             }
         }
-
+        
         return resHearts;
     }
 }
