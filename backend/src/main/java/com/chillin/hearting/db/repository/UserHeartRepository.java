d4450ebc1324ed1f8f34f5b6139a9dfe2fced62a
손민혁 <sonmh79@naver.com>
2023-04-28 02:48:19 +0900

    feat: add comment(native query)

--- backend/src/main/java/com/chillin/hearting/db/repository/UserHeartRepository.java
+++ backend/src/main/java/com/chillin/hearting/db/repository/UserHeartRepository.java
@@ -8,4 +8,10 @@ import java.util.List;
 public interface UserHeartRepository extends JpaRepository<UserHeart, Long> {
 
     List<UserHeart> findAllByUserId(String userId);
+
+//    @Query(value = "select h.id, h.name, h.image_url, h.short_description, h.long_description, h.type, h.acq_condition, uh.user_id " +
+//            "from user_heart as uh left outer join heart as h " +
+//            "on h.id = uh.heart_id " +
+//            "where uh.user_id = :userId", nativeQuery = true)
+//    public List<Heart> findMyHearts(@Param("userId") String userId);
 }
