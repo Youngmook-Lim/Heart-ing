106f591d07988723cd23ba4a5f2503604ef8a4ff
손민혁 <sonmh79@naver.com>
2023-04-28 02:50:16 +0900

    feat: add DTO for heart detail

---
+++ backend/src/main/java/com/chillin/hearting/api/data/HeartDetailData.java
@@ -0,0 +1,21 @@
+package com.chillin.hearting.api.data;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
+public class HeartDetailData implements Data {
+    private Long heartId;
+    private String name;
+    private String heartUrl;
+    private String shortDescription;
+    private String longDescription;
+    private String type;
+    private String acqCondition;
+    private String isLocked;
+    private String sendCnt;
+    private String receivedCnt;
+}
