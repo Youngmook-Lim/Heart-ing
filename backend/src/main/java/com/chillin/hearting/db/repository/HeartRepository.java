7c19dee551634f064ff817ed403a0f83eaf80abd
손민혁 <sonmh79@naver.com>
2023-04-28 02:47:11 +0900

    feat: add new query

--- backend/src/main/java/com/chillin/hearting/db/repository/HeartRepository.java
+++ backend/src/main/java/com/chillin/hearting/db/repository/HeartRepository.java
@@ -3,7 +3,11 @@ package com.chillin.hearting.db.repository;
 import com.chillin.hearting.db.domain.Heart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HeartRepository extends JpaRepository<Heart, Long> {
+
+    public List<Heart> findAllByType(String default_type);
 
 }
 
