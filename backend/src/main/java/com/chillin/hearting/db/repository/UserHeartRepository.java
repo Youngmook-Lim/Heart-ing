a0dd7dd9dc58551abc446e29c5083d7258016c63
sonmh79 <sonmh79@naver.com>
2023-04-27 17:32:36 +0900

    fix: fix method to use userId

--- backend/src/main/java/com/chillin/hearting/db/repository/UserHeartRepository.java
+++ backend/src/main/java/com/chillin/hearting/db/repository/UserHeartRepository.java
@@ -1,6 +1,5 @@
 package com.chillin.hearting.db.repository;
 
-import com.chillin.hearting.db.domain.User;
 import com.chillin.hearting.db.domain.UserHeart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +7,5 @@ import java.util.List;
 
 public interface UserHeartRepository extends JpaRepository<UserHeart, Long> {
 
-    public List<UserHeart> findByUser(User user);
+    List<UserHeart> findAllByUserId(String userId);
 }
