9d8e68ee1ed09676c53bb64d552c3871d64d79f8
leegaeun <rkdms4054@gmail.com>
2023-04-28 01:00:21 +0900

    feat: add heartguide api

--- frontend/src/pages/HeartGuide.tsx
+++ frontend/src/pages/HeartGuide.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { getAllHeartInfo, getHeartDetailInfo } from '../features/api/guide'
+import { useRecoilValue } from 'recoil'
+import { openDetailInfoAtom } from '../atoms/guideAtoms'
+import { IHeartDetailInfoTypes } from '../types/guideType'
+import HeartGuideList from '../components/heartGuide/HeartGuideList'
+import HeartGuideDetailInfo from '../components/heartGuide/HeartGuideDetailInfo'
 
 function HeartGuide() {
+
+  const [allHeartInfoList, setAllHeartInfoList] = useState([])
+  const [heartDetailInfo, setHeartDetailInfo] = useState<IHeartDetailInfoTypes | null>(null)
+  const openDetailInfo = useRecoilValue(openDetailInfoAtom)
+
+
+  async function getHeartData() {
+    const allHeartInfoList = await getAllHeartInfo()
+    setAllHeartInfoList(allHeartInfoList)
+  }
+
+  async function handleGetHeartDetailInfo(heartId: number) {
+    const heartDetailInfoData = await getHeartDetailInfo(heartId)
+    setHeartDetailInfo(heartDetailInfoData)
+  }
+
+  useEffect(() => {
+    getHeartData()
+  }, [])
+  
+
   return (
     <div>
-      하트도감
+      <HeartGuideList allHeartInfoList={allHeartInfoList} onGetHeartDetailData={handleGetHeartDetailInfo}/>
+      {openDetailInfo ? <HeartGuideDetailInfo heartDetailInfo={heartDetailInfo} /> : null }
     </div>
   )
 }
