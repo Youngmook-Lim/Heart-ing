c2e68194a9ae8288c12d6ed8533701e7bbdb4c81
KooooY <8nay.nyanya8@gmail.com>
2023-04-27 17:50:19 +0900

    feat: add reissue api

--- frontend/src/features/api/https.ts
+++ frontend/src/features/api/https.ts
@@ -1,5 +1,5 @@
 import baseAxios from "axios";
-import { getUserInfo } from "../userInfo";
+import { deleteUserInfo, getUserInfo } from "../userInfo";
 
 export const axios = baseAxios.create({
   baseURL: process.env.REACT_APP_API,
@@ -12,3 +12,24 @@ axios.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${getUserInfo().accessToken}`;
   return config;
 });
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response && error.response.status) {
+      if (error.response.status === 401) {
+        deleteUserInfo()
+        alert("다시 로그인해주세요");
+        console.log('강종')
+        window.location.replace("/login");
+        // ForcedLogout()
+        return new Promise(() => {});
+      } else {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
