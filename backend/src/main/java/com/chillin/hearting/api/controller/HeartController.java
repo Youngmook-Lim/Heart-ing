ebbe738823bbfc078fae1c6da719887f573943f1
손민혁 <sonmh79@naver.com>
2023-04-28 02:46:01 +0900

    fix: fix heart controller

--- backend/src/main/java/com/chillin/hearting/api/controller/HeartController.java
+++ backend/src/main/java/com/chillin/hearting/api/controller/HeartController.java
@@ -1,16 +1,22 @@
 package com.chillin.hearting.api.controller;
 
+import com.chillin.hearting.api.data.HeartData;
+import com.chillin.hearting.api.data.HeartListData;
 import com.chillin.hearting.api.response.ResponseDTO;
 import com.chillin.hearting.api.service.HeartService;
+import com.chillin.hearting.db.domain.Heart;
 import com.chillin.hearting.db.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -19,18 +25,30 @@ import javax.servlet.http.HttpServletRequest;
 public class HeartController {
 
     private final HeartService heartService;
+    private final static String MESSAGE_SUCCESS = "success";
+    private final static String FIND_ALLHEARTS_SUCCESS = "도감용 하트 리스트 조회에 성공했습니다.";
+    private final static String FIND_MSGHEARTS_SUCCESS = "메시지용 하트 리스트 조회에 성공했습니다.";
 
     @GetMapping("/all")
     public ResponseEntity<ResponseDTO> findAllHearts(HttpServletRequest httpServletRequest) {
         User user = (User) httpServletRequest.getAttribute("user");
-        heartService.findAllHearts(user);
-        return null;
+        List<HeartData> allHearts = heartService.findAllHearts(user);
+        System.out.println(allHearts.size());
+        ResponseDTO responseDTO = ResponseDTO.builder().status(MESSAGE_SUCCESS).message(FIND_ALLHEARTS_SUCCESS).data(HeartListData.builder().hearts(allHearts).build()).build();
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
     @GetMapping("/message")
     public ResponseEntity<ResponseDTO> findMessageHearts(HttpServletRequest httpServletRequest) {
         User user = (User) httpServletRequest.getAttribute("user");
-        heartService.findMessageHearts(user);
+        List<HeartData> messageHearts = heartService.findMessageHearts(user);
+        ResponseDTO responseDTO = ResponseDTO.builder().status(MESSAGE_SUCCESS).message(FIND_MSGHEARTS_SUCCESS).data(HeartListData.builder().hearts(messageHearts).build()).build();
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/{heartId}")
+    public ResponseEntity<ResponseDTO> findMessageHearts(@PathVariable Long heartId) {
+        Heart findHeart = heartService.findDetailHeart(heartId);
         return null;
     }
 }
