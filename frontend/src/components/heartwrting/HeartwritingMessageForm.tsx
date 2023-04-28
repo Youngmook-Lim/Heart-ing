790dd2f9ddd72e01f03a73b44cc0933e6e9b1776
KooooY <8nya.nyanya8@gmail.com>
2023-04-29 03:12:47 +0900

    fix: change message submit logic

--- frontend/src/components/heartwrting/HeartwritingMessageForm.tsx
+++ frontend/src/components/heartwrting/HeartwritingMessageForm.tsx
@@ -5,47 +5,44 @@ import { getUserInfo } from '../../features/userInfo';
 function HeartwritingMessageForm({...props}) {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
-
-  const onSubmitHandler = async(e: React.FocusEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const messageInfo: IMessageSendTypes = {
-      heartId: Number(props.selectedHeart),
-      senderId: getUserInfo().userId,
-      receiverId: props.userId,
-      title: title,
-      content: content,
-    }
-
-    props.onSendingHandler(messageInfo)
-  } 
-
+  
   const onTitleHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const currentTitle = e.currentTarget.value
-    setTitle(currentTitle)
+    if (e.currentTarget.value.length > 8) {
+      const currentTitle = e.currentTarget.value.substr(0, 8);
+      props.setTitle(currentTitle)
+      setTitle(currentTitle)
+    } else {
+      const currentTitle = e.currentTarget.value
+      props.setTitle(currentTitle)
+      setTitle(currentTitle)
+    }
   }
 
   const onContentHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const currentContent = e.currentTarget.value
-    setContent(currentContent)
+    if (e.currentTarget.value.length > 100) {
+      const currentContent = e.currentTarget.value.substr(0, 100);
+      props.setContent(currentContent)
+      setContent(currentContent)
+    } else {
+      const currentContent = e.currentTarget.value
+      props.setContent(currentContent)
+      setContent(currentContent)
+    }
   }
 
   return (
     <div>
-      <form onSubmit={onSubmitHandler}>
-        <input type="text" className='w-72 mt-5 mb-2 text-center border-b-2 border-hrtColorPink' onChange={onTitleHandler} placeholder='제목을 입력해주세요'/>
-        <br/>
-        <div className='w-72 mx-auto text-left mb-5'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="inline w-4 h-4 mx-1">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-          </svg>
-          <span className='text-sm text-hrtColorPink'>
-            전달하는 마음은 24시간 동안 유지되며, 수정•삭제할 수 없습니다
-          </span>
-        </div>
-        <textarea className="block w-72 h-32 text-center mx-auto py-2 border-2 border-hrtColorPink outline-none" onChange={onContentHandler} placeholder='전하고 싶은 마음이 있다면,&#13;&#10;메세지를 작성해보세요'/>
-        <br/>
-      </form>
+      <input type="text" value={title} className='w-72 mt-5 mb-2 text-center border-b-2 border-hrtColorPink outline-none' onChange={onTitleHandler} placeholder='제목을 입력해주세요'/>
+      <br/>
+      <div className='w-72 mx-auto text-left mb-5'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="inline w-4 h-4 mx-1">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+        </svg>
+        <span className='text-sm text-hrtColorPink'>
+          전달하는 마음은 24시간 동안 유지되며, 수정•삭제할 수 없습니다
+        </span>
+      </div>
+      <textarea value={content} className="block w-72 h-40 text-center mx-auto py-2 mb-10 border-2 border-hrtColorPink outline-none rounded-md" onChange={onContentHandler} placeholder='전하고 싶은 마음이 있다면,&#13;&#10;메세지를 작성해보세요'/>
     </div>
   )
 }
--- frontend/src/pages/Heartwriting.tsx
+++ frontend/src/pages/Heartwriting.tsx
@@ -15,6 +15,8 @@ function Heartwriting() {
   const [heartList, setHeartList] = useState([])
   const [isSelected, setIsSelected] = useState(false)
   const [selectedHeartId, setSelectedHeartId] = useState('')
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
   
   let params = new URL(document.URL).searchParams;
   let userId = params.get("id");
@@ -34,13 +36,23 @@ function Heartwriting() {
     [navigate]
   );
 
-  async function sendMessage(messageInfo: IMessageSendTypes) {
+  const onSendHandler = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!userId) return;
+    const messageInfo: IMessageSendTypes = {
+      heartId: Number(selectedHeartId),
+      senderId: getUserInfo().userId,
+      receiverId: userId,
+      title: title,
+      content: content,
+    }
+    
     const status = await sendMessageApi(messageInfo)
     if (status === 'success') {
       alert('메세지 성공!')
       navigate(`/heartboard/user?id=${userId}`)
     }
   }
+ 
 
   async function getHeartList() {
     const data = await getMessageHeartApi()
@@ -71,13 +83,13 @@ function Heartwriting() {
             마음 보내기
           </div>
           {isSelected ?
-            <HeartwritingMessage onSendingHandler={sendMessage} onSettingMode={setMode} selectedHeartInfo={heartList[Number(selectedHeartId)-1]} userId={userId}/>
+            <HeartwritingMessage setTitle={setTitle} setContent={setContent} onSettingMode={setMode} selectedHeartInfo={heartList[Number(selectedHeartId)-1]} userId={userId}/>
             : 
             <HeartwritingSelectHeart onHeartNumberHandler={setHeartNumber} onSettingMode={setMode} heartList={heartList}/>
           }
         </div>
       {isSelected ?
-        <button className="bg-hrtColorYellow px-8 h-16 w-48 rounded-xl border-2 border-hrtColorPink shadow-[0_4px_4px_rgba(251,139,176,1)] my-5">
+        <button onClick={onSendHandler} className="bg-hrtColorYellow px-8 h-16 w-48 rounded-xl border-2 border-hrtColorPink shadow-[0_4px_4px_rgba(251,139,176,1)] my-5">
           <div className="text-2xl">전달하기</div>
         </button>
         // null
