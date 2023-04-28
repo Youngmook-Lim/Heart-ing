7442b11c86a99eca256ca06e6fdf29f6ba989043
leegaeun <rkdms4054@gmail.com>
2023-04-28 16:35:14 +0900

    feat: moved dummy data #28

--- frontend/src/components/heartGuide/HeartGuideDetailInfoStory.tsx
+++ frontend/src/components/heartGuide/HeartGuideDetailInfoStory.tsx
@@ -12,10 +12,6 @@ function HeartGuideDetailInfoStory({ heartStory }:HeartGuideDetailInfoStoryProps
           <p>스토리</p>
         </div>
         {/* <div>{ heartStory }</div> */}
-        <div className="border-2">
-          <p>두려움을 이길 용기를 전할 때 태어난 하트</p>
-          <p>너의 하늘을 날아봐! 더 높은 세상을 꿈꾸는 상대에게 날개를 달아준다.</p>
-        </div>
       </>
     )
     }
