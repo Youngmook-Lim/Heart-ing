f0229198273d602ab5b6e05d71ec334e9f948353
Youngmook Lim <youngmookk@gmail.com>
2023-04-28 20:56:40 +0900

    test: add fail tests for controllers

--- backend/src/test/java/com/chillin/hearting/api/controller/MessageControllerTest.java
+++ backend/src/test/java/com/chillin/hearting/api/controller/MessageControllerTest.java
@@ -5,9 +5,7 @@ import com.chillin.hearting.api.request.ReportReq;
 import com.chillin.hearting.api.request.SendMessageReq;
 import com.chillin.hearting.api.service.MessageService;
 import com.chillin.hearting.db.domain.User;
-import com.chillin.hearting.exception.ControllerExceptionHandler;
-import com.chillin.hearting.exception.UserNotFoundException;
-import com.chillin.hearting.exception.WrongUserException;
+import com.chillin.hearting.exception.*;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -149,6 +147,86 @@ class MessageControllerTest {
     }
 
     @Test
+    public void failDeleteMessage_InvalidUser() throws Exception {
+        // given
+        final String url = "/api/v1/messages/" + messageId;
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.delete(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isUnauthorized()).andExpect(jsonPath("$.message", is(UnAuthorizedException.DEFAULT_MESSAGE)));
+    }
+
+    @Test
+    public void failDeleteMessage_ServerFail() throws Exception {
+        // given
+        final String url = "/api/v1/messages/" + messageId;
+        User user = User.builder().id("receiver").build();
+        doReturn(true).when(messageService).deleteMessage(messageId, "receiver");
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.delete(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("user", user);
+                            return request;
+                        })
+        );
+
+        // then
+        resultActions.andExpect(status().isInternalServerError()).andExpect(jsonPath("$.message", is(DeleteMessageFailException.DEFAULT_MESSAGE)));
+    }
+
+    @Test
+    public void failDeleteMessage_UnauthorizedUser() throws Exception {
+        // given
+        final String url = "/api/v1/messages/" + messageId;
+        User user = User.builder().id("sender").build();
+        doThrow(new UnAuthorizedException("본인에게 온 메시지만 삭제할 수 있습니다."))
+                .when(messageService).deleteMessage(messageId, "sender");
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.delete(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("user", user);
+                            return request;
+                        })
+        );
+
+        // then
+        resultActions.andExpect(status().isUnauthorized()).andExpect(jsonPath("$.message", is("본인에게 온 메시지만 삭제할 수 있습니다.")));
+    }
+
+    @Test
+    public void failDeleteMessage_AlreadyDeleted() throws Exception {
+        // given
+        final String url = "/api/v1/messages/" + messageId;
+        User user = User.builder().id("sender").build();
+        doThrow(new MessageAlreadyDeletedException())
+                .when(messageService).deleteMessage(messageId, "sender");
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.delete(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("user", user);
+                            return request;
+                        })
+        );
+
+        // then
+        resultActions.andExpect(status().isBadRequest()).andExpect(jsonPath("$.message", is(MessageAlreadyDeletedException.DEFAULT_MESSAGE)));
+    }
+
+    @Test
     public void successDeleteMessage() throws Exception {
         // given
         final String url = "/api/v1/messages/" + messageId;
@@ -171,6 +249,102 @@ class MessageControllerTest {
         resultActions.andExpect(status().isOk())
                 .andExpect(jsonPath("$.message", is("메시지가 성공적으로 삭제되었습니다.")))
                 .andExpect(jsonPath("$.status", is(SUCCESS)));
+    }
+
+    @Test
+    public void failReportMessage_InvalidUser() throws Exception {
+        // given
+        final String url = "/api/v1/messages/" + messageId + "/reports";
+        ReportReq reportReq = ReportReq.builder()
+                .content(content)
+                .build();
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .content(gson.toJson(reportReq))
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isUnauthorized()).andExpect(jsonPath("$.message", is(UnAuthorizedException.DEFAULT_MESSAGE)));
+    }
+
+    @Test
+    public void failReportMessage_ServerFail() throws Exception {
+        // given
+        final String url = "/api/v1/messages/" + messageId + "/reports";
+        User user = User.builder().id("sender").build();
+        ReportReq reportReq = ReportReq.builder()
+                .content(content)
+                .build();
+        doReturn(null).when(messageService).reportMessage(messageId, "sender", content);
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .content(gson.toJson(reportReq))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("user", user);
+                            return request;
+                        })
+        );
+
+        // then
+        resultActions.andExpect(status().isInternalServerError()).andExpect(jsonPath("$.message", is(ReportFailException.DEFAULT_MESSAGE)));
+    }
+
+    @Test
+    public void failReportMessage_UnauthorizedUser() throws Exception {
+        // given
+        final String url = "/api/v1/messages/" + messageId + "/reports";
+        User user = User.builder().id("sender").build();
+        ReportReq reportReq = ReportReq.builder()
+                .content(content)
+                .build();
+        doThrow(new UnAuthorizedException("본인이 받은 메시지만 신고할 수 있습니다."))
+                .when(messageService).reportMessage(messageId, "sender", content);
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .content(gson.toJson(reportReq))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("user", user);
+                            return request;
+                        })
+        );
+
+        // then
+        resultActions.andExpect(status().isUnauthorized()).andExpect(jsonPath("$.message", is("본인이 받은 메시지만 신고할 수 있습니다.")));
+    }
+
+    @Test
+    public void failReportMessage_AlreadyReported() throws Exception {
+        // given
+        final String url = "/api/v1/messages/" + messageId + "/reports";
+        User user = User.builder().id("sender").build();
+        ReportReq reportReq = ReportReq.builder()
+                .content(content)
+                .build();
+        doThrow(new MessageAlreadyReportedException())
+                .when(messageService).reportMessage(messageId, "sender", content);
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .content(gson.toJson(reportReq))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("user", user);
+                            return request;
+                        })
+        );
+
+        // then
+        resultActions.andExpect(status().isBadRequest()).andExpect(jsonPath("$.message", is(MessageAlreadyReportedException.DEFAULT_MESSAGE)));
     }
 
     @Test
@@ -201,6 +375,64 @@ class MessageControllerTest {
         resultActions.andExpect(status().isCreated())
                 .andExpect(jsonPath("$.message", is("메시지가 성공적으로 신고되었습니다.")))
                 .andExpect(jsonPath("$.status", is(SUCCESS)));
+    }
+
+    @Test
+    public void failAddEmoji_InvalidUser() throws Exception {
+        // given
+        final String url = "/api/v1/messages/" + messageId + "/emojis/" + emojiId;
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isUnauthorized()).andExpect(jsonPath("$.message", is(UnAuthorizedException.DEFAULT_MESSAGE)));
+    }
+
+    @Test
+    public void failAddEmoji_ServerFail() throws Exception {
+        // given
+        final String url = "/api/v1/messages/" + messageId + "/emojis/" + emojiId;
+        User user = User.builder().id("sender").build();
+        doReturn(null).when(messageService).addEmoji(messageId, "sender", emojiId);
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("user", user);
+                            return request;
+                        })
+        );
+
+        // then
+        resultActions.andExpect(status().isInternalServerError()).andExpect(jsonPath("$.message", is(EmojiFailException.DEFAULT_MESSAGE)));
+    }
+
+    @Test
+    public void failAddEmoji_UnauthorizedUser() throws Exception {
+        // given
+        final String url = "/api/v1/messages/" + messageId + "/emojis/" + emojiId;
+        User user = User.builder().id("sender").build();
+        doThrow(new UnAuthorizedException("본인이 받은 메시지에만 이모지를 달 수 있습니다."))
+                .when(messageService).addEmoji(messageId, "sender", emojiId);
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("user", user);
+                            return request;
+                        })
+        );
+
+        // then
+        resultActions.andExpect(status().isUnauthorized()).andExpect(jsonPath("$.message", is("본인이 받은 메시지에만 이모지를 달 수 있습니다.")));
     }
 
     @Test
--- backend/src/test/java/com/chillin/hearting/api/controller/MessageReceivedControllerTest.java
+++ backend/src/test/java/com/chillin/hearting/api/controller/MessageReceivedControllerTest.java
@@ -5,7 +5,9 @@ import com.chillin.hearting.api.data.ReceivedMessageData;
 import com.chillin.hearting.api.service.MessageReceivedService;
 import com.chillin.hearting.db.domain.User;
 import com.chillin.hearting.exception.ControllerExceptionHandler;
+import com.chillin.hearting.exception.MessageDetailFailException;
 import com.chillin.hearting.exception.ReceivedMessagesListFailException;
+import com.chillin.hearting.exception.UnAuthorizedException;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,6 +26,7 @@ import java.util.ArrayList;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -56,11 +59,11 @@ class MessageReceivedControllerTest {
     }
 
     @Test
-    public void failGetReceivedMessages_ListFail() throws Exception {
+    public void failGetReceivedMessages_ServerFail() throws Exception {
         // given
         final String url = "/api/v1/messages/received/" + "receiver";
-        User user = User.builder().id("otherSender").build();
-        doReturn(null).when(messageReceivedService).getReceivedMessages("receiver", false);
+        User user = User.builder().id("receiver").build();
+        doReturn(null).when(messageReceivedService).getReceivedMessages("receiver", true);
 
         // when
         final ResultActions resultActions = mockMvc.perform(
@@ -74,7 +77,6 @@ class MessageReceivedControllerTest {
 
         // then
         resultActions.andExpect(status().isInternalServerError()).andExpect(jsonPath("$.message", is(ReceivedMessagesListFailException.DEFAULT_MESSAGE)));
-
     }
 
     @Test
@@ -105,6 +107,65 @@ class MessageReceivedControllerTest {
                 .andExpect(jsonPath("$.data", notNullValue()))
                 .andExpect(jsonPath("$.message", is("받은메시지 리스트가 성공적으로 반환되었습니다.")))
                 .andExpect(jsonPath("$.status", is(SUCCESS)));
+    }
+
+    @Test
+    public void failGetMessageDetail_ServerFail() throws Exception {
+        // given
+        final String url = "/api/v1/messages/received/detail/" + messageId;
+        User user = User.builder().id("receiver").build();
+        doReturn(null).when(messageReceivedService).getMessageDetail(messageId, "receiver");
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("user", user);
+                            return request;
+                        })
+        );
+
+        // then
+        resultActions.andExpect(status().isInternalServerError()).andExpect(jsonPath("$.message", is(MessageDetailFailException.DEFAULT_MESSAGE)));
+
+    }
+
+    @Test
+    public void failMessageDetail_InvalidUser() throws Exception {
+        // given
+        final String url = "/api/v1/messages/received/detail/" + messageId;
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isUnauthorized()).andExpect(jsonPath("$.message", is(UnAuthorizedException.DEFAULT_MESSAGE)));
+    }
+
+    @Test
+    public void failMessageDetail_UnauthorizedUser() throws Exception {
+        // given
+        final String url = "/api/v1/messages/received/detail/" + messageId;
+        User user = User.builder().id("sender").build();
+        doThrow(new UnAuthorizedException("본인의 메시지만 상세열람할 수 있습니다."))
+                .when(messageReceivedService).getMessageDetail(messageId, "sender");
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("user", user);
+                            return request;
+                        })
+        );
+
+        // then
+        resultActions.andExpect(status().isUnauthorized()).andExpect(jsonPath("$.message", is("본인의 메시지만 상세열람할 수 있습니다.")));
     }
 
     @Test
