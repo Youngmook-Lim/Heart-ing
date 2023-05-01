04092f82b12d6a5e0ac4b500b4ec1ec4299050b1
손민혁 <sonmh79@naver.com>
2023-05-02 01:40:00 +0900

    feat: add find sent messages method

--- backend/src/main/java/com/chillin/hearting/api/service/MessageSentService.java
+++ backend/src/main/java/com/chillin/hearting/api/service/MessageSentService.java
@@ -1,9 +1,18 @@
 package com.chillin.hearting.api.service;
 
+import com.chillin.hearting.api.data.SentMessageData;
+import com.chillin.hearting.api.data.SentMessageListData;
+import com.chillin.hearting.db.domain.Message;
+import com.chillin.hearting.db.repository.SentMessageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -11,4 +20,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class MessageSentService {
 
+    private final SentMessageRepository sentMessageRepository;
+
+    public SentMessageListData getSentMessages(String userId) {
+        List<Message> findMessages = sentMessageRepository.findBySenderIdAndExpiredDateAfterOrderByExpiredDate(userId, LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+        List<SentMessageData> sendMessageDatas = new ArrayList<>();
+        for (Message message : findMessages) {
+            sendMessageDatas.add(SentMessageData.of(message));
+        }
+        return SentMessageListData.builder().sentMessageDataList(sendMessageDatas).build();
+    }
 }
