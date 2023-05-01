77a6142ec47c3e2f06ff549548ee161a76783f6e
손민혁 <sonmh79@naver.com>
2023-05-02 02:15:03 +0900

    feat: add get sent message detail method

--- backend/src/main/java/com/chillin/hearting/api/service/MessageSentService.java
+++ backend/src/main/java/com/chillin/hearting/api/service/MessageSentService.java
@@ -1,9 +1,12 @@
 package com.chillin.hearting.api.service;
 
+import com.chillin.hearting.api.data.Data;
 import com.chillin.hearting.api.data.SentMessageData;
 import com.chillin.hearting.api.data.SentMessageListData;
 import com.chillin.hearting.db.domain.Message;
 import com.chillin.hearting.db.repository.SentMessageRepository;
+import com.chillin.hearting.exception.MessageAlreadyExpiredException;
+import com.chillin.hearting.exception.MessageNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -29,5 +32,15 @@ public class MessageSentService {
             sendMessageDatas.add(SentMessageData.of(message));
         }
         return SentMessageListData.builder().sentMessageList(sendMessageDatas).build();
+    }
+
+    public Data getSentMessageDetail(String userId, Long messageId) {
+        Message findMessage = sentMessageRepository.findByIdAndSenderId(messageId, userId).orElseThrow(MessageNotFoundException::new);
+        if (findMessage.getExpiredDate().isBefore(LocalDateTime.now(ZoneId.of("Asia/Seoul")))) {
+            throw new MessageAlreadyExpiredException();
+        }
+        SentMessageData detailMessage = SentMessageData.of(findMessage);
+        detailMessage.setContent(findMessage.getContent());
+        return detailMessage;
     }
 }
