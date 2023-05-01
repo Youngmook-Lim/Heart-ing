2f879a353f379c1b6d04b953beace7478c67f12a
손민혁 <sonmh79@naver.com>
2023-05-02 02:14:25 +0900

    feat: add sent message detail in repo

--- backend/src/main/java/com/chillin/hearting/db/repository/SentMessageRepository.java
+++ backend/src/main/java/com/chillin/hearting/db/repository/SentMessageRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface SentMessageRepository extends JpaRepository<Message, Long> {
     List<Message> findBySenderIdAndExpiredDateAfterOrderByExpiredDate(String userId, LocalDateTime now);
+
+    Optional<Message> findByIdAndSenderId(Long messageId, String userId);
 }
