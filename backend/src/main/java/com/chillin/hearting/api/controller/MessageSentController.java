2ac2b88d7baf74b4c77f90bae3715419a3402240
손민혁 <sonmh79@naver.com>
2023-05-01 23:27:01 +0900

    feat: add sent message controller

---
+++ backend/src/main/java/com/chillin/hearting/api/controller/MessageSentController.java
@@ -0,0 +1,14 @@
+package com.chillin.hearting.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/messages/sent")
+@RequiredArgsConstructor
+public class MessageSentController {
+
+}
