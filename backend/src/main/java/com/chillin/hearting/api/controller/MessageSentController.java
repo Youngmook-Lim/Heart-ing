0ca4ce44e07e5e1c9add544d135d21c2f452936e
손민혁 <sonmh79@naver.com>
2023-05-02 01:40:41 +0900

    feat: add get sent messages controller

--- backend/src/main/java/com/chillin/hearting/api/controller/MessageSentController.java
+++ backend/src/main/java/com/chillin/hearting/api/controller/MessageSentController.java
@@ -1,9 +1,18 @@
 package com.chillin.hearting.api.controller;
 
+import com.chillin.hearting.api.data.Data;
+import com.chillin.hearting.api.response.ResponseDTO;
+import com.chillin.hearting.api.service.MessageSentService;
+import com.chillin.hearting.db.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
@@ -11,4 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MessageSentController {
 
+    private static final String SUCCESS = "success";
+    private final MessageSentService messageSentService;
+
+    @GetMapping("")
+    public ResponseEntity<ResponseDTO> getSentMessages(HttpServletRequest httpServletRequest) {
+
+        User user = (User) httpServletRequest.getAttribute("user");
+
+        Data data = messageSentService.getSentMessages(user.getId());
+
+        ResponseDTO responseDTO = ResponseDTO.builder()
+                .status(SUCCESS)
+                .message("보낸 메시지 리스트가 성공적으로 반환되었습니다.")
+                .data(data)
+                .build();
+
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
 }
