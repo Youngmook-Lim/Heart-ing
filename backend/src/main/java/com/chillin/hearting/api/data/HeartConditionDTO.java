1d2f9ca5cc1bf82aac601542c9ffeccde6a61a25
sonmh79 <sonmh79@naver.com>
2023-05-02 16:35:24 +0900

    feat: add heart condition DTO

---
+++ backend/src/main/java/com/chillin/hearting/api/data/HeartConditionDTO.java
@@ -0,0 +1,11 @@
+package com.chillin.hearting.api.data;
+
+public interface HeartConditionDTO {
+    Long getHeartId();
+
+    String getName();
+
+    String getHeartUrl();
+
+    int getCurrentValue();
+}
