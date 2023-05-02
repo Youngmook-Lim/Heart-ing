a01ed2ceb600cbb736f141f1ee329d80a8695978
haegomm <rkdms4054@gmail.com>
2023-05-03 03:58:51 +0900

    feat: add conditions list and object type

--- frontend/src/types/guideType.ts
+++ frontend/src/types/guideType.ts
@@ -6,15 +6,22 @@ export interface IHeartInfoTypes {
     isLocked: boolean,
 }
 
+export interface IHeartConditionsTypes{
+    heartId: number | null,
+    name: string | null,
+    heartUrl: string | null,
+    currentValue: number,
+    maxValue: number,
+}
+
 export interface IHeartDetailInfoTypes {
     heartId: number,
     name: string,
-    imageUrl: string,
+    heartUrl: string,
     shortDescription : string,
     longDescription : string,
     type : string,
     acqCondition : string,
     isLocked : boolean,
-    sendCnt : number,
-    receivedCnt : number,
+    conditions: IHeartConditionsTypes[] | null,
 }
