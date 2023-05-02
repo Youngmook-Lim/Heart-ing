bbb41da2198f2456a31fec178803f2500cc9b86e
sonmh79 <sonmh79@naver.com>
2023-05-02 16:38:31 +0900

    feat: add heart detail service logic

--- backend/src/main/java/com/chillin/hearting/api/service/HeartService.java
+++ backend/src/main/java/com/chillin/hearting/api/service/HeartService.java
@@ -1,12 +1,11 @@
 package com.chillin.hearting.api.service;
 
-import com.chillin.hearting.api.data.Data;
-import com.chillin.hearting.api.data.HeartData;
-import com.chillin.hearting.api.data.HeartDetailData;
+import com.chillin.hearting.api.data.*;
 import com.chillin.hearting.db.domain.Heart;
 import com.chillin.hearting.db.domain.User;
 import com.chillin.hearting.db.domain.UserHeart;
 import com.chillin.hearting.db.repository.HeartRepository;
+import com.chillin.hearting.db.repository.MessageHeartConditionRepository;
 import com.chillin.hearting.db.repository.UserHeartRepository;
 import com.chillin.hearting.exception.HeartNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -28,9 +27,12 @@ public class HeartService {
     private final HeartRepository heartRepository;
 
     private final UserHeartRepository userHeartRepository;
+    private final MessageHeartConditionRepository messageHeartConditionRepository;
 
     private static final String DEFAULT_TYPE = "DEFAULT";
     private static final HashSet<Long> lockedHeartSet = new HashSet<>(Arrays.asList(4L, 5L));
+    private static final int HEART_PLANET_MAX_VALUE = 5;
+    private static final int HEART_RAINBOW_MAX_VALUE = 1;
 
     /**
      * 모든 도감 리스트를 반환합니다.
@@ -98,12 +100,49 @@ public class HeartService {
             if (user != null) {
                 String userId = user.getId();
                 List<UserHeart> findUserHeart = userHeartRepository.findByHeartIdAndUserId(heartId, userId);
-                if (findUserHeart.size() > 0) {
+                if (!findUserHeart.isEmpty()) {
                     heartDetailData.setIsLocked(false);
+                } else {
+                    List<HeartConditionData> conditionList = getHeartCondition(userId, findHeart);
+                    heartDetailData.setConditions(conditionList);
                 }
             }
         }
 
         return heartDetailData;
+    }
+
+    private List<HeartConditionData> getHeartCondition(String userId, Heart heart) {
+        int heartId = heart.getId().intValue();
+        List<HeartConditionData> heartConditionList = new ArrayList<>();
+        switch (heartId) {
+            case 6:
+                log.debug("행성 하트 획득 조건을 반환합니다.");
+
+                HeartConditionData heartConditionData = HeartConditionData.of(null);
+                int userCurrentValue = messageHeartConditionRepository.findMaxMessageCountToSameUser(userId);
+                heartConditionData.setCurrentValue(userCurrentValue);
+                heartConditionData.setMaxValue(HEART_PLANET_MAX_VALUE);
+                heartConditionList.add(heartConditionData);
+
+            case 7:
+                log.debug("무지개 하트 획득 조건을 반환합니다.");
+                List<Heart> defaultHearts = heartRepository.findAllByType(DEFAULT_TYPE);
+                List<HeartConditionDTO> resultList = heartRepository.findDefaultHeartSentCount(userId);
+
+                int size = resultList.size();
+                for (HeartConditionDTO dto : resultList) {
+                    HeartConditionData data = HeartConditionData.builder()
+                            .heartId(dto.getHeartId())
+                            .name(dto.getName())
+                            .heartUrl(dto.getHeartUrl())
+                            .currentValue((dto.getCurrentValue() > 1) ? HEART_RAINBOW_MAX_VALUE : dto.getCurrentValue())
+                            .maxValue(HEART_RAINBOW_MAX_VALUE)
+                            .build();
+                    heartConditionList.add(data);
+                }
+        }
+
+        return heartConditionList;
     }
 }
