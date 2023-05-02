ebc21ea874e0f77e409a42e9d8a3afa96d88e27e
KooooY <8nya.nyanya8@gmail.com>
2023-05-03 00:56:08 +0900

    feat: connect socket.io

--- frontend/src/components/navbar/NavbarNotification.tsx
+++ frontend/src/components/navbar/NavbarNotification.tsx
@@ -1,18 +1,41 @@
 import React, { useEffect, useRef } from "react";
+import { useRecoilValue } from "recoil";
+import { io } from 'socket.io-client';
+import { isLoginAtom } from "../../atoms/userAtoms";
+import { getUserInfo } from "../../features/userInfo";
 
 function NavbarNotification({...props}) {
   const notiRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    const handleClick = (e: MouseEvent) => {
-      console.log('커런트',notiRef.current)
-      if (notiRef.current && !notiRef.current.contains(e.target as Node)) {
-        props.onNotiHandler();
-        console.log('바깥 눌렀다')
-      }
-    };
-    window.addEventListener('mousedown', handleClick);
-    return () => window.removeEventListener('mousedown', handleClick);
-  }, [notiRef]);
+  const isLogin = useRecoilValue(isLoginAtom)
+
+  const onSocket = () => {
+    if (isLogin) {
+      const socket = io("https://heart-ing.com", { path: "/ws" });
+      socket.on("connect", () => {
+        console.log("웹소켓 서버에 연결");
+        socket.emit('join-room', getUserInfo().userId);
+      })
+      socket.on("receive-message", (data) => {
+        console.log("받은 메시지:", data);
+      });
+    }
+
+  };
+
+    useEffect(() => {
+      onSocket()
+    }, [])
+  // useEffect(() => {
+  //   const handleClick = (e: MouseEvent) => {
+  //     console.log('커런트',notiRef.current)
+  //     if (notiRef.current && !notiRef.current.contains(e.target as Node)) {
+  //       props.onNotiHandler();
+  //       console.log('바깥 눌렀다')
+  //     }
+  //   };
+  //   window.addEventListener('mousedown', handleClick);
+  //   return () => window.removeEventListener('mousedown', handleClick);
+  // }, [notiRef]);
 
   return (
     <div>
