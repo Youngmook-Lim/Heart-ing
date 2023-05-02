b4014d8c1a2360d0983fe46fd2caf3699fdc90cb
KooooY <8nay.nyanya8@gmail.com>
2023-05-02 17:52:39 +0900

    feat: add notification div

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import NavbarSide from "./NavbarSide";
 import NavbarSideContext from "./NavbarSideContext";
 
@@ -7,18 +7,31 @@ import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { isLoginAtom } from "../../atoms/userAtoms";
 import { getUserInfo } from "../../features/userInfo";
+import useDetectClose from "../../features/hook/useDetectClose";
+import NavbarNotification from "./NavbarNotification";
 
 function Navbar() {
   const navigate = useNavigate()
+  
   const isLogin = useRecoilValue(isLoginAtom)
+  // const [notiIsOpen, notiRef, notiHandler] = useDetectClose(false)
+  const [notiIsOpen, setNotiIsOpen] = useState(false)
+  const myId = getUserInfo().userId
 
+  const onNotiHandler = (e: MouseEvent) => {
+    setNotiIsOpen(!notiIsOpen)
+  }
+
+  const onNotiButtonHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+    setNotiIsOpen(!notiIsOpen)
+  }
+  
   const onNavigateHandler = (e: React.MouseEvent<HTMLDivElement>) => {
-    {isLogin ? 
+    isLogin ? 
       navigate(`/heartboard/user?id=${getUserInfo().userId}`)
-      :
-      navigate('/manual')
+      : navigate('/manual')
   }
-  }
+  
   return (
     <div>
       <nav className="">
@@ -26,22 +39,30 @@ function Navbar() {
           <div onClick={onNavigateHandler}>
             <img src={Logo} alt="test" className="w-16 m-2" />
           </div>
-          <div className="w-6 m-2 my-4 flex-none">
-            <svg
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                clipRule="evenodd"
-                fillRule="evenodd"
-                d="M10 2a6 6 0 00-6 6c0 1.887-.454 3.665-1.257 5.234a.75.75 0 00.515 1.076 32.91 32.91 0 003.256.508 3.5 3.5 0 006.972 0 32.903 32.903 0 003.256-.508.75.75 0 00.515-1.076A11.448 11.448 0 0116 8a6 6 0 00-6-6zM8.05 14.943a33.54 33.54 0 003.9 0 2 2 0 01-3.9 0z"
-              ></path>
-            </svg>
-          </div>
+          {isLogin ? 
+          <div>
+            <div 
+              className="w-6 m-2 my-4 flex-none"
+              onClick={onNotiButtonHandler}>
+              <svg
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                  d="M10 2a6 6 0 00-6 6c0 1.887-.454 3.665-1.257 5.234a.75.75 0 00.515 1.076 32.91 32.91 0 003.256.508 3.5 3.5 0 006.972 0 32.903 32.903 0 003.256-.508.75.75 0 00.515-1.076A11.448 11.448 0 0116 8a6 6 0 00-6-6zM8.05 14.943a33.54 33.54 0 003.9 0 2 2 0 01-3.9 0z"
+                ></path>
+              </svg>
+            </div>
+            {notiIsOpen ? 
+            <NavbarNotification onNotiHandler={onNotiHandler} />
+            : null}
+            </div>
+            :null}
         </div>
-
         <NavbarSide width={60}>
           <NavbarSideContext />
         </NavbarSide>
