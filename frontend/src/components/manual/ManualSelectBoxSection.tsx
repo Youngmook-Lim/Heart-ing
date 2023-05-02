6a09c7d520be23348bec8bb69b2fadfdd14b592e
haegomm <rkdms4054@gmail.com>
2023-05-03 00:00:52 +0900

    feat: add section component #46

--- frontend/src/components/manual/ManualSelectBoxSection.tsx
+++ frontend/src/components/manual/ManualSelectBoxSection.tsx
@@ -1,18 +1,22 @@
-import heart_select from '../../assets/images/png/heart_select.png'
+import ManualSelectBoxSectionImg from "./ManualSelectBoxSectionImg"
 
-function ManualSelectBoxSection() {
+interface ManualSelectBoxSectionProps {
+  name: string,
+  content: string,
+  manualImg: number
+}
+
+function ManualSelectBoxSection({name, content, manualImg}: ManualSelectBoxSectionProps) {
 
   return (
     <div className="flex flex-col items-center">
       <div className="flex justify-center items-center">
-        <img src={heart_select} alt="heart_select" className="w-3/5 mx-auto mt-6" />
+        <ManualSelectBoxSectionImg manualImg={manualImg} />
       </div>
       <div>
         <div className='m-4'>
-          <p className='text-lg font-bold'>하트전달</p>
-          <p className='text-sm whitespace-pre-wrap'>상대에게 전달하고 싶은 하트를 선택해보세요.
-            전달하고 싶은 메세지가 있다면 함께 보낼 수도 있어요.
-            전달하는 하트는 24시간만 유지된다는 것을 주의하세요!
+          <p className='text-lg font-bold mb-4'>{name}</p>
+          <p className='text-sm w-full mx-auto px-8'>{content}
           </p>
         </div>
       </div>
