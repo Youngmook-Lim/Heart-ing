df7dc2560d2a8ffd428baf06bc6938943b060ca2
sonmh79 <sonmh79@naver.com>
2023-05-02 16:39:15 +0900

    feat: add heart condition check query

--- backend/src/main/java/com/chillin/hearting/db/repository/HeartRepository.java
+++ backend/src/main/java/com/chillin/hearting/db/repository/HeartRepository.java
@@ -1,13 +1,21 @@
 package com.chillin.hearting.db.repository;
 
+import com.chillin.hearting.api.data.HeartConditionDTO;
 import com.chillin.hearting.db.domain.Heart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
 
     public List<Heart> findAllByType(String default_type);
+
+    @Query(value = "select id as heartId, name, image_url as heartUrl, ifnull((select count(*) from message where sender_id=:userId and heart_id=h.id group by heart_id),0) as currentValue " +
+            "from heart as h " +
+            "where type = 'DEFAULT'", nativeQuery = true)
+    public List<HeartConditionDTO> findDefaultHeartSentCount(@Param(value = "userId") String userId);
 
 }
 
