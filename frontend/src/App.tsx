1514699e128d1c9e00edc1f9874e80a5d3ecd495
seohyeonkyeong <hkyung99@naver.com>
2023-05-03 14:41:30 +0900

    feat: interactive full screen size

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,8 +17,15 @@ import Navbar from "./components/navbar/Navbar";
 import Google from "./pages/Google";
 
 function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`); //"--vh"라는 속성으로 정의해준다.
+  }
+
+  window.addEventListener("resize", () => setScreenSize());
+
   return (
-    <div className="App bg-hrtColorBackground text-hrtColorOutline h-full min-h-screen">
+    <div className="App bg-hrtColorBackground text-hrtColorOutline">
       <BrowserRouter>
         <Navbar />
         <Routes>
