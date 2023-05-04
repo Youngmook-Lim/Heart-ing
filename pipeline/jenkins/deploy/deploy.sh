a28a7e354f0bb194dfbcfa0a8759947b1328e9de
Youngmook Lim <youngmookk@gmail.com>
2023-05-04 15:52:34 +0900

    fix: fix mistake

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -11,6 +11,11 @@ echo "*DEPLOYING KUBERNETES DEPLOYMENTS*"
 echo "**********************************"
 
 cd /var/jenkins_home/workspace/hearting-pipeline-docker/pipeline/jenkins/deploy/k8s
+
+for file in hearting-back.yml hearting-front.yml hearting-websocket.yml; do
+  sed -i "s/__PRODUCTION_VERSION__/${PRODUCTION_VERSION}/g" $file
+done
+
 kubectl --kubeconfig=/var/jenkins_home/kubeconfig.yml apply -f .
 
 # delete dangling images
