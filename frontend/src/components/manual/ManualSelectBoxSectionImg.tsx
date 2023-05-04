803c27b938167df27f083c38bec2ccda7134c3ad
leegaeun <rkdms4054@gmail.com>
2023-05-04 16:55:47 +0900

    feat: add img

--- frontend/src/components/manual/ManualSelectBoxSectionImg.tsx
+++ frontend/src/components/manual/ManualSelectBoxSectionImg.tsx
@@ -1,4 +1,12 @@
+import hearting_logo_line from "../../assets/images/logo/logo_line.png"
 import heart_select from "../../assets/images/png/heart_select.png";
+import heart_send from "../../assets/images/png/heart_send.png";
+import heart_sent from "../../assets/images/png/heart_sent.png";
+import heart_received from "../../assets/images/png/heart_received.png";
+import heart_save from "../../assets/images/png/heart_save.png";
+import share from "../../assets/images/png/share.png";
+import heart_rainbow_1 from "../../assets/images/png/heart_rainbow_1.png";
+import condition from "../../assets/images/png/condition.png";
 
 interface ManualSelectBoxSectionImgProps {
   manualImg: number;
@@ -9,21 +17,22 @@ function ManualSelectBoxSectionImg({
 }: ManualSelectBoxSectionImgProps) {
   const index = manualImg;
   const manualImgList = [
-    heart_select,
-    heart_select,
-    heart_select,
-    heart_select,
-    heart_select,
-    heart_select,
-    heart_select,
-    heart_select,
-    heart_select,
-    heart_select,
+    hearting_logo_line,
+    hearting_logo_line,
+    heart_send,
+    heart_sent,
+    heart_received,
+    heart_save,
+    share,
+    heart_rainbow_1,
+    condition, 
   ];
   return (
     <>
-      <div className="text-center flex justify-center h-full">
-        <img src={manualImgList[index]} alt="설명서 이미지" className="" />
+      <div className="text-center mx-auto flex justify-center h-full">
+        <div className="h-12">
+          <img src={manualImgList[index]} alt="설명서 이미지" className="h-56" />
+        </div>
       </div>
     </>
   );
--- frontend/src/components/modal/MessageModal.tsx
+++ frontend/src/components/modal/MessageModal.tsx
@@ -95,7 +95,7 @@ function MessageModal({ mode }: IMessageModalTypes) {
     const isExpired = kr_curr > expiredDate; // 현재 시간보다 만료 시간이 더 과거이면 만료됨
     return (
       <div className="App w-full fixed left-0 top-0 bg-black bg-opacity-30 text-center flex  items-center justify-center z-40">
-        <div className="container modal border-hrtColorOutline m-6 w-full">
+        <div className="container modal border-hrtColorOutline m-6 max-w-xs">
           <div className="modal-header bg-hrtColorOutline border-hrtColorOutline mb-4 flex">
             <div className="flex-auto">하트 읽기</div>
             <button onClick={() => closeModal()} className="flex-none">
