f86ecbffb1c51b365de339563d103c5eb90a7a5d
leegaeun <rkdms4054@gmail.com>
2023-05-04 12:38:29 +0900

    feat: add emoji api handler

--- frontend/src/components/heartResponse/HeartResponseEmojiList.tsx
+++ frontend/src/components/heartResponse/HeartResponseEmojiList.tsx
@@ -1,7 +1,8 @@
+import { useEffect } from "react"
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import HeartResponseEmojiListItem from "./HeartResponseEmojiListItem"
 import touch_arrow from "../../assets/images/png/touch_arrow.png"
-import { isOpenEmojiListAtom } from "../../atoms/messageAtoms"
-import { useSetRecoilState } from "recoil";
+import { isOpenEmojiListAtom, isSelectedEmojiIdAtom } from "../../atoms/messageAtoms"
 
 interface propsType {
   onEmojiHandler: (emojiId: number) => void;
@@ -9,22 +10,29 @@ interface propsType {
 
 function HeartResponseEmojiList({ onEmojiHandler }: propsType) {
 
+  const isSelectedEmojiId = useRecoilValue(isSelectedEmojiIdAtom)
   const setIsOpenEmojiList = useSetRecoilState(isOpenEmojiListAtom)
 
   const closeModalEmojiBox = () => {
     setIsOpenEmojiList(false);
   };
 
+  useEffect(() => {
+    return () => {
+      onEmojiHandler(isSelectedEmojiId)
+    }
+  }, [])
+
   return (
-    <>
-      <div className="heartBoard border-hrtColorPink relative">
-        <div className="w-auto bg-hrtColorPink border-hrtColorPink flex justify-between z-40">
+    <div className="flex flex-col w-full">
+      <div className="heartBoard border-hrtColorPink z-40">
+        <div className="w-auto bg-hrtColorPink border-hrtColorPink flex justify-between">
           <div className="text-white text-sm mx-2">반응하기</div>
           <button className="w-6 text-white" onClick={closeModalEmojiBox}>x</button>
         </div>
         <div className="grid grid-cols-5 py-2">
           {Array.from({ length: 5 }, (_, index) => (
-            <HeartResponseEmojiListItem key={index} id={index + 1} onEmojiHandler={onEmojiHandler} />
+            <HeartResponseEmojiListItem key={index} id={index + 1} />
           ))}
         </div>
       </div>
@@ -32,7 +40,7 @@ function HeartResponseEmojiList({ onEmojiHandler }: propsType) {
         <img src={touch_arrow} alt="touch_arrow"
         className="w-6" />
       </div>
-    </>
+    </div>
   )
 }
 
