3bd15fb796d1e349eadc03e1956e7c5966346724
Youngmook Lim <youngmookk@gmail.com>
2023-05-04 12:41:17 +0900

    feat: add kubernetes apply to build.sh

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -8,3 +8,10 @@ docker compose up -d
 
 # delete dangling images
 docker image prune -f
+
+echo "**********************************"
+echo "*DEPLOYING KUBERNETES DEPLOYMENTS*"
+echo "**********************************"
+
+cd /var/jenkins_home/workspace/hearting-pipeline-docker/pipeline/jenkins/deploy/k8s
+kubectl --kubeconfig=/var/jenkins_home/kubeconfig.yml apply -f .
