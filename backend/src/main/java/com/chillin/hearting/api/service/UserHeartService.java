694805de9c0f9aa76cd1089d347e55b52125b601
sonmh79 <sonmh79@naver.com>
2023-05-04 16:33:11 +0900

    feat: throw exception on already acquired user heart

--- backend/src/main/java/com/chillin/hearting/api/service/UserHeartService.java
+++ backend/src/main/java/com/chillin/hearting/api/service/UserHeartService.java
@@ -6,10 +6,13 @@ import com.chillin.hearting.db.domain.UserHeart;
 import com.chillin.hearting.db.repository.HeartRepository;
 import com.chillin.hearting.db.repository.UserHeartRepository;
 import com.chillin.hearting.db.repository.UserRepository;
+import com.chillin.hearting.exception.HeartAlreadyAcquiredException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -27,7 +30,8 @@ public class UserHeartService {
         User findUser = userRepository.findById(userId).get();
         Heart findHeart = heartRepository.findById(heartId).get();
         UserHeart userHeart = new UserHeart(findUser, findHeart);
-
+        List<UserHeart> findUserHeart = userHeartRepository.findByHeartIdAndUserId(heartId, userId);
+        if (!findUserHeart.isEmpty()) throw new HeartAlreadyAcquiredException();
         userHeartRepository.save(userHeart);
     }
 }
