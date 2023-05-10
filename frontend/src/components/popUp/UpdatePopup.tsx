809d6b4e8242e25ed2d1fa3a9971ae6363ce4db5
KooooY <8nay.nyanya8@gmail.com>
2023-05-10 15:00:12 +0900

    fix: fix update modal css

--- frontend/src/components/popUp/UpdatePopup.tsx
+++ frontend/src/components/popUp/UpdatePopup.tsx
@@ -12,14 +12,16 @@ function UpdatePopup() {
                     <div className="bg-hrtColorNewPurple border-hrtColorNewPurple mb-4 flex">
                         <div className="flex-auto text-xl text-hrtColorOutline font-bold py-2">⭐ 업 데 이 트 &nbsp; 알 림 ⭐</div>
                     </div>
-                    <div className="text-left px-16 py-2">
-                        <p className="text-xl">v2.0.10 업데이트 사항</p>
-                        <br></br>
-                        <p>• 메세지 보내기 버그 해결</p>
-                        <p>• 행성하트 지급 완료</p>
-                        <p>• 알람 기능 추가</p>
-                        <p>• 프로필 수정 페이지 추가</p>
-                        <p>• SNS 공유하기 추가</p>
+                    <div className="flex justify-center">
+                        <div className="text-left py-2">
+                            <p className="text-xl">v2.0.10 업데이트 사항</p>
+                            <br></br>
+                            <p>• 메세지 보내기 버그 해결</p>
+                            <p>• 행성하트 지급 완료</p>
+                            <p>• 알람 기능 추가</p>
+                            <p>• 프로필 수정 페이지 추가</p>
+                            <p>• SNS 공유하기 추가</p>
+                        </div>
                     </div>
                     <div className="mx-auto my-auto mt-5 mb-4 modal-button text-hrtColorOutline"
                         onClick={() => closeUpdateForever()}>
