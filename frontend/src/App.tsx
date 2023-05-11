83934abde620462878a7e9965133c7d6b6ef583f
haegomm <rkdms4054@gmail.com>
2023-05-12 01:51:39 +0900

    feat: delete reporting import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,6 @@ import Google from "./pages/Google";
 import { Socket, io } from "socket.io-client";
 import { useRecoilValue } from "recoil";
 import { isLoginAtom } from "./atoms/userAtoms";
-import Reporting from "./components/reporting/Reporting";
 
 declare global {
   interface Window {
@@ -76,7 +75,6 @@ function App() {
           <Route path="/receivedheart" element={<ReceivedHeart />} />
           <Route path="/sentheart" element={<SentHeart />} />
           <Route path="/profilesettings" element={<Settings />} />
-          <Route path="/reporting" element={<Reporting />} />
         </Routes>
       </BrowserRouter>
     </div>
