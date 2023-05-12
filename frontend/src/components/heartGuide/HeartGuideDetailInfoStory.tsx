3a9b11a558410b5a7b6b2ff34ce80b92bb1a48df
leegaeun <rkdms4054@gmail.com>
2023-05-12 13:17:31 +0900

    feat: add cursor css

--- frontend/src/components/heartGuide/HeartGuideDetailInfoStory.tsx
+++ frontend/src/components/heartGuide/HeartGuideDetailInfoStory.tsx
@@ -7,9 +7,9 @@ function HeartGuideDetailInfoStory({ heartStory }:HeartGuideDetailInfoStoryProps
     return (
       <>
         <div className="textShadow">
-          <p className="purple text-white text-left tracking-wider mx-1">스토리</p>
+          <p className="purple text-white text-left tracking-wider mx-1 cursor-default">스토리</p>
         </div>
-        <div className="border-2 border-hrtColorNewPurple p-4 mt-1 text-start rounded-sm">{ heartStory }</div>
+        <div className="border-2 border-hrtColorNewPurple p-4 mt-1 text-start rounded-sm cursor-default">{ heartStory }</div>
       </>
     )
     }
