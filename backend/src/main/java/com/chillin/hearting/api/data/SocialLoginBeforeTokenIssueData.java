8d42e4dce97cd1867d564ff91b999750f6721923
황정주 <hjj950303@naver.com>
2023-05-17 03:50:46 +0900

    refactor: add description of OAuthService class, method, rename SoialLoginResultData class to SocailLoginBeforeTokenIssueData #111

--- backend/src/main/java/com/chillin/hearting/api/data/SocialLoginBeforeTokenIssueData.java
+++ backend/src/main/java/com/chillin/hearting/api/data/SocialLoginBeforeTokenIssueData.java
@@ -10,7 +10,7 @@ import lombok.*;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class SocialLoginResultData {
+public class SocialLoginBeforeTokenIssueData {
 
     private User user;
 
