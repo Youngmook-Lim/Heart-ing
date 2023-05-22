26902265f06ee15d6c0735375332fd021566627c
손민혁 <sonmh79@naver.com>
2023-05-23 05:05:08 +0900

    feat: add redis config

--- backend/src/main/java/com/chillin/hearting/config/RedisConfig.java
+++ backend/src/main/java/com/chillin/hearting/config/RedisConfig.java
@@ -3,12 +3,19 @@ package com.chillin.hearting.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class RedisConfig {
@@ -43,5 +50,37 @@ public class RedisConfig {
         return redisTemplate;
     }
 
+    /**
+     * Redis Cache 설정
+     * Map을 이용하여 캐시 키를 등록
+     * 모든 키에 180초로 설정
+     */
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofSeconds(180)) // default 만료 시간
+                .serializeKeysWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        Map<String, RedisCacheConfiguration> cacheConfigurationMap = new HashMap<>();
+
+        // entryTtl 설정
+        cacheConfigurationMap.put("user",
+                redisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(180)));
+        cacheConfigurationMap.put("entry",
+                redisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(180)));
+
+        return RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
 
 }
